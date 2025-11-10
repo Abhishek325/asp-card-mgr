04c4b6769b495e17be1756389fdf46764dd755b5
Abhishek Singh <abhishekk@AbSarahs-MacBook-Pro.local>
2025-11-10 12:29:22 +0530

    Added gh-pages configuration

--- quasar.conf.js
+++ quasar.conf.js
@@ -47,7 +47,8 @@ module.exports = configure(function (/* ctx */) {
 
     // Full list of options: https://v1.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'hash', // available values: 'hash', 'history',
+      publicPath: '/asp-card-mgr/',
 
       // transpile: false,
 
